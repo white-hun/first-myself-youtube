9d15fd73304374f97e93cb609501be13d597913e
white-hun <exwhite@naver.com>
2023-03-29 22:00:19 +0900

    Update route

--- first-myself-youtube/src/App.js
+++ first-myself-youtube/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Root from "./components/Root";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "/home", element: <Home /> },
+      // {path: "", element: }
+      // {path: "", element: }
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
---
+++ first-myself-youtube/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navdar() {
+  return (
+    <div>
+      <Link to="/home">test home</Link>
+      {/* <Link to="">test </Link> */}
+    </div>
+  );
+}
