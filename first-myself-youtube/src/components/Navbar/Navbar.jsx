6c83b75e20679023c6ba8abf0558d7cacb309bc6
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-30 10:07:05 +0900

    Update

--- first-myself-youtube/src/components/Navbar/Navbar.jsx
+++ first-myself-youtube/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./Root.module.css";
+import styles from "./Navbar.module.css";
 
 export default function Navdar() {
   return (
     <nav className={styles.navbar}>
       <Link to="/home">test home</Link>
-      <Link to="/video">test page</Link>
     </nav>
   );
 }
