f1b21b05cdcf6369031c7c1125c21f87ed2f6237
white-hun <exwhite@naver.com>
2023-04-02 16:52:46 +0900

    Update search page

--- first-myself-youtube/src/pages/SearchVideoCard/SearchVideoCard.jsx
+++ first-myself-youtube/src/pages/SearchVideoCard/SearchVideoCard.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import styles from "./SearchVideoCard.module.css";
+import { Link } from "react-router-dom";
 
-export default function SearchVideoCard({ title }) {
-  return <div>{title}</div>;
+export default function SearchVideoCard({ id, thumbnails, title, pubilsh, channel, description }) {
+  return (
+    <div className={styles.searchvideocard}>
+      <Link to={`/videos/${id}`}>
+        <div>
+          <img src={thumbnails.url} alt={title} className={styles.thumbnails} />
+        </div>
+        <div>
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.publish}>날짜 왜 안나옴</p>
+          <h4 className={styles.channel}>{channel}</h4>
+          <div className={styles.description}>{description}</div>
+        </div>
+      </Link>
+    </div>
+  );
 }
--- first-myself-youtube/src/pages/VIdeoSearch/VideoSearch.jsx
+++ first-myself-youtube/src/pages/VIdeoSearch/VideoSearch.jsx
@@ -3,17 +3,33 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import SearchVideoCard from "../SearchVideoCard/SearchVideoCard";
 import NotFound from "../NotFound/NotFound";
+import styles from "./VideoSearch.module.css";
 
 export default function VideoSearch() {
   const { search } = useParams();
-  const { data: bts } = useQuery(["bts"], async () => {
-    return fetch("data/bts.json").then((res) => res.json());
+  const {
+    isLoading,
+    error,
+    data: bts,
+  } = useQuery(["bts"], async () => {
+    return fetch(`/../../data/${search}.json`).then((res) => res.json());
   });
 
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <div>
+    <div className={styles.videosearch}>
       {bts.items.map((value) => (
-        <SearchVideoCard title={value.snippet.title} />
+        <SearchVideoCard
+          key={value.id.videoId}
+          id={value.id.videoId}
+          thumbnails={value.snippet.thumbnails.medium}
+          title={value.snippet.title}
+          pubilsh={value.snippet.pubilshedAt}
+          channel={value.snippet.channelTitle}
+          description={value.snippet.description}
+        />
       ))}
     </div>
   );
