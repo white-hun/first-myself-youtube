35744e6fc6942d0cd95563bf0d4c79fa363d07b7
white-hun <121800818+white-hun@users.noreply.github.com>
2023-04-04 18:03:10 +0900

    Update

--- first-myself-youtube/src/pages/VideoDetail/VideoDetail.jsx
+++ first-myself-youtube/src/pages/VideoDetail/VideoDetail.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import styles from "./VideoDetail.module.css";
 
 export default function VideoDetail() {
-  const { id } = useParams();
+  const { etag } = useParams();
   const {
     isLoading,
     error,
@@ -15,20 +15,14 @@ export default function VideoDetail() {
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
-  return (
-    <div>
-      {all.items.map((prev) => (
-        <div>
-          <img
-            className={styles.divbox}
-            src={prev.snippet.thumbnails.medium.url}
-            alt={prev.snippet.title}
-          />
-          <p>{prev.snippet.title}</p>
-        </div>
-      ))}
-    </div>
-  );
+  return <div>{all.items.filter((prev) => prev.etag !== etag)}</div>;
 }
 
 //
+{
+  /* <img
+  className={styles.divbox}
+  src={prev.includes(etag).snippet.thumbnails.medium.url}
+  alt={prev.includes(etag).snippet.title}
+/> */
+}
