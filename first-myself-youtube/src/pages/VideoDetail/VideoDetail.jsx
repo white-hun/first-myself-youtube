797d03316612c27e356d3c9ae7550752125df525
white-hun <121800818+white-hun@users.noreply.github.com>
2023-04-05 15:27:38 +0900

    Update

--- first-myself-youtube/src/pages/VideoDetail/VideoDetail.jsx
+++ first-myself-youtube/src/pages/VideoDetail/VideoDetail.jsx
@@ -8,19 +8,24 @@ export default function VideoDetail() {
   const {
     isLoading,
     error,
-    data: all,
-  } = useQuery(["all"], async () => {
-    return fetch(`../../data/popular.json`).then((res) => res.json());
+    data: test,
+  } = useQuery(["test"], async () => {
+    return fetch("../../data/popular.json").then((res) => res.json());
   });
+  // const jsonData = JSON.parse(test);
+  const videoData = test.items.find((item) => item.etag === etag);
+  const thumbnail = videoData.snippet.thumbnails.medium.url;
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
-  return <div>{all.items.filter((prev) => prev.etag !== etag)}</div>;
+
+  return (
+    <div className={styles.divbox}>
+      {etag}
+      {thumbnail ? <img src={thumbnail} alt="test" /> : null}
+    </div>
+  );
 }
 
-//
-//  <img
-//   className={styles.divbox}
-//   src={prev.includes(etag).snippet.thumbnails.medium.url}
-//   alt={prev.includes(etag).snippet.title}
-// />
+// 여기에 컴포넌트 속성을 만들고
+// 다른 파일에 속성을 받아서 보여준다
